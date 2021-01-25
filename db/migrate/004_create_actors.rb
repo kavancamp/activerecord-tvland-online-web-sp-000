912f05000070be826c4bfc00cf74ace90c68bdec
Keenah Vancampenhout <kvancamp@mail.com>
2021-01-25 11:39:58 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,6 +1,6 @@
 class CreateActors < ActiveRecord::Migration[5.1]
   def change
-    create_table :actors do |t|
+    create_table :actors do |a|
       a.string :first_name
       a.string :last_name
     end
