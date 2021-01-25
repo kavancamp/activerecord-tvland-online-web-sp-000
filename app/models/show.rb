cccf9602ffddd1088f428d596e9ffa0216cff0be
Keenah Vancampenhout <kvancamp@mail.com>
2021-01-25 11:44:29 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,8 +2,12 @@ class Show < ActiveRecord::Base
   has_many :characters
     has_many :actors, through: :characters
     belongs_to :network
-
+    
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+      end
+      
     def actors_list
-    self.actors.collect {|actor| "#{actor.name} - #{actor.show.name}"}.join(", ")
+    self.actors.collect {|actor| "#{actor.first_name} - #{actor.last_name}"}.join(", ")
   end
 end
